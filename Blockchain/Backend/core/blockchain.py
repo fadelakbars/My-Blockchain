36aeb6c1f171ab22434f06484764179ecb66ef76
Fadhel Akbar <fadhelakbarsallang@gmail.com>
2025-03-29 00:50:12 +0800

    #4 write blockchain data on disk

--- Blockchain/Backend/core/blockchain.py
+++ Blockchain/Backend/core/blockchain.py
@@ -4,16 +4,23 @@ sys.path.append("/BLOCKCHAIN/Codies_Alert/Bitcoin")
 from Blockchain.Backend.core.block import Block
 from Blockchain.Backend.core.blockheader import BlockHeader
 from Blockchain.Backend.util.util import hash256
+from Blockchain.Backend.core.database.database import BlockchainDB
 import time
-import json
 
 ZERO_HASH = "0" * 64
 VERSION = 1
 
 class Blockchain:
     def __init__(self):
-        self.chain = []
         self.GenesisBlock()
+        
+    def write_on_disk(self, block):
+        blockchainDB = BlockchainDB()
+        blockchainDB.write(block)
+        
+    def fetch_last_block(self):
+        blockchainDB = BlockchainDB()
+        return blockchainDB.lastBlock()
         
     def GenesisBlock(self):
         BlockHeight = 0
@@ -26,17 +33,15 @@ class Blockchain:
         Transaction = f"Fadel mengirm {BlockHeight} Bitcoin ke Akbar"
         merkleRoot = hash256(Transaction.encode()).hex()
         bits = 'fff0001f'
-        # blockheader = BlockHeader(VERSION, prevBlockHash, timestamp, merkleRoot, bits)
         blockheader = BlockHeader(VERSION, prevBlockHash, merkleRoot, timestamp, bits)
         blockheader.mine()
-        self.chain.append(Block(BlockHeight, 1, blockheader.__dict__, 1, Transaction).__dict__)
-        print(json.dumps(self.chain, indent= 4))
+        self.write_on_disk([Block(BlockHeight, 1, blockheader.__dict__, 1, Transaction).__dict__])
         
     def main(self):
         while True:
-            lastBlock = self.chain[::-1]
-            BlockHeight = lastBlock[0]["Height"]+1
-            prevBlockHash = lastBlock[0]['BlockHeader']['blockHash']
+            lastBlock = self.fetch_last_block()
+            BlockHeight = lastBlock["Height"]+1
+            prevBlockHash = lastBlock['BlockHeader']['blockHash']
             self.addBlock(BlockHeight, prevBlockHash)
         
 if __name__ == "__main__":
