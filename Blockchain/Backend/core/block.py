d07ee265997e0237767c323308213a50a786c018
Fadhel Akbar <fadhelakbarsallang@gmail.com>
2025-03-28 23:16:27 +0800

    # 3 create blockchain in python under 30 minutes

---
+++ Blockchain/Backend/core/block.py
@@ -0,0 +1,11 @@
+class Block:
+    """
+        Block adalah wadah untuk menyimpan transaksi  
+    """
+    
+    def __init__(self, Height, Blocksize, BlockHeader, TxCount, Txs):
+        self.Height = Height
+        self.Blocksize = Blocksize
+        self.BlockHeader = BlockHeader
+        self.Txcount = TxCount
+        self.Txs = Txs
